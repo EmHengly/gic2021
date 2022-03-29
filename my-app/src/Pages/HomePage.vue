63855d89e39994097a1cec9906c5b8ea2f90c0de
unknown <emhengly@gmail.com>
2022-03-29 09:23:20 +0700

    todo-app

--- my-app/src/Pages/HomePage.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <h1>This is home page</h1>
-        <router-link to="/pag1">Page 1</router-link>
-        <router-link to="/page2">Page 2</router-link>
-        <router-link to="/page3">Page 3</router-link>
-    </div>
-</template>
--- my-app/src/Pages/Page_1.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        
-        <router-link to="/page2">Page 2</router-link>
-        <router-link to="/page3">Page 3</router-link>
-        <h1>Page 1</h1>
-    </div>
-</template>
