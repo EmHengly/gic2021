0d1232cc3135acdb44279325bf56fd5806456fd8
unknown <emhengly@gmail.com>
2022-03-29 09:20:39 +0700

    todo-app

---
+++ my-app/src/Pages/Page_1.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        
+        <router-link to="/page2">Page 2</router-link>
+        <router-link to="/page3">Page 3</router-link>
+        <h1>Page 1</h1>
+    </div>
+</template>
---
+++ my-app/src/Pages/Page_3.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <router-link to="/page1">Page 1</router-link>
+        <router-link to="/page2">Page 2</router-link>
+        <h1>Page 3</h1>
+    </div>
+</template>
